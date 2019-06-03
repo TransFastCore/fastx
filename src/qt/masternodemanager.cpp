e427844b702342095e6478ab1c24ada3dc42662a
TransFastCore <51037903+TransFastCore@users.noreply.github.com>
2019-06-03 09:32:49 +0200

    1.0.0.0

--- src/qt/masternodemanager.cpp
+++ src/qt/masternodemanager.cpp
@@ -124,9 +124,9 @@ void MasternodeManager::updateAdrenalineNode(QString alias, QString addr, QStrin
 
     ui->tableWidget_2->setItem(nodeRow, 0, aliasItem);
     ui->tableWidget_2->setItem(nodeRow, 1, addrItem);
-    ui->tableWidget_2->setItem(nodeRow, 2, rewardPercentageItem);
-    ui->tableWidget_2->setItem(nodeRow, 3, rewardAddressItem);
-    ui->tableWidget_2->setItem(nodeRow, 4, statusItem);
+    //ui->tableWidget_2->setItem(nodeRow, 2, rewardPercentageItem);
+    //ui->tableWidget_2->setItem(nodeRow, 3, rewardAddressItem);
+    ui->tableWidget_2->setItem(nodeRow, 2, statusItem);
 }
 
 static QString seconds_to_DHMS(quint32 duration)
