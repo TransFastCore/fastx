25c61e31db7b5c628c1ec64cfbb51566b488d208
TransFastCore <51037903+TransFastCore@users.noreply.github.com>
2019-10-20 20:16:43 +0200

    1.1.1.0

--- src/darksend.h
+++ src/darksend.h
@@ -240,7 +240,7 @@ public:
 class CDarkSendSigner
 {
 public:
-    /// Is the inputs associated with this public key? (and there is 5000 or 10000 or 20000 - checking if valid masternode)
+    /// Is the inputs associated with this public key?
     bool IsVinAssociatedWithPubkey(CTxIn& vin, CPubKey& pubkey);
     /// Set the private/public key values, returns true if successful
     bool SetKey(std::string strSecret, std::string& errorMessage, CKey& key, CPubKey& pubkey);
